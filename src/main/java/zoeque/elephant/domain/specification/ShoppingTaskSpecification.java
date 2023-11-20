d9b0c5a3f811fee92efcfa27f8d49c86ec4ca23e
zoeque <121230485+zoeque@users.noreply.github.com>
2023-11-20 19:53:34 +0900

    #4 - Create specification, delete mail service that might be managed as one util project

---
+++ src/main/java/zoeque/elephant/domain/specification/ShoppingTaskSpecification.java
@@ -0,0 +1,46 @@
+package zoeque.elephant.domain.specification;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import zoeque.elephant.domain.entity.ShoppingTask;
+import zoeque.elephant.domain.entity.ShoppingTask_;
+import zoeque.elephant.domain.model.NotificationStatusModel;
+import zoeque.elephant.domain.valueobject.TaskExecutionDate_;
+
+/**
+ * The entity specification for {@link ShoppingTask}.
+ */
+@Component
+public class ShoppingTaskSpecification {
+
+  /**
+   * The specification that is not reported and
+   * task must be executed today.
+   *
+   * @return The specification with the above status.
+   */
+  public Specification<ShoppingTask> itemToNotify() {
+    return new Specification<ShoppingTask>() {
+      @Override
+      public Predicate toPredicate(Root<ShoppingTask> root, CriteriaQuery<?> query,
+                                   CriteriaBuilder criteriaBuilder) {
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(criteriaBuilder.lessThanOrEqualTo(
+                root.get(ShoppingTask_.EXECUTION_DATE)
+                        .get(TaskExecutionDate_.DATE_TIME),
+                LocalDateTime.now()
+        ));
+        predicates.add(criteriaBuilder.equal(root.get(ShoppingTask_.STATUS),
+                NotificationStatusModel.PLANNED));
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+      }
+    };
+  }
+}
