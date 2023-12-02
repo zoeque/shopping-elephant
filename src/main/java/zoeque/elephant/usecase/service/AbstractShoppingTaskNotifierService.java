40d63e6d972aea45798bd75aeba4cc8297effe8a
zoeque <121230485+zoeque@users.noreply.github.com>
2023-12-02 22:06:19 +0900

    #5 - update the message body builder

--- src/main/java/zoeque/elephant/usecase/service/AbstractShoppingTaskNotifierService.java
+++ src/main/java/zoeque/elephant/usecase/service/AbstractShoppingTaskNotifierService.java
@@ -2,6 +2,7 @@ package zoeque.elephant.usecase.service;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import zoeque.elephant.configuration.SettingConfiguration;
+import zoeque.mailer.application.event.MailRequestEvent;
 
 public abstract class AbstractShoppingTaskNotifierService
         implements IShoppingTaskHandleService {
