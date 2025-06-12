bc3750c05e48541e9db6750a19cb53ca05545a71
ry <121230485+zoeque@users.noreply.github.com>
2025-06-12 12:01:06 +0900

    update #48 - drop controller

---
+++ shopping-elephant/src/controller/ShoppingTaskDropController.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const sendPostRequest = async (identifier: Int16Array, itemName: string, executionDate: string) => {
+
+    try {
+         await axios.post('http://localhost:8080/drop', {
+            identifier: identifier,
+            itemName: itemName,
+            executionDate: executionDate
+        });
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
